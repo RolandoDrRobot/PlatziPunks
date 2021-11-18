16f69d9210c2e72a698e84c0fbd462149ceb8825
RolandoDrRobot <rarguello@ltvco.com>
2021-11-17 18:50:02 -0600

    Adding NTF 721 template / create our custom deploy file

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const deploy = async () => {
+
+  // Brings me the address and private key that is allowed to sign
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying the contract', deployer.address);
+
+
+  // We create an instancec of the contract to deploy
+  const PlatziPunks = await ethers.getContractFactory("PlatziPunks");
+  // We created an instance of the deployed contract
+  const deployed = await PlatziPunks.deploy();
+  console.log('PlatziPunks is deployed at:', deployed.address);
+};
+
+deploy().then(() => process.exit(0)).catch(error => {
+  console.log(error);
+  process.exit(1);
+});
